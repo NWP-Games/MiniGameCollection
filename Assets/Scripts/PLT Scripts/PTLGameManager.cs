ad7be6905ccce187405379318374d890af820134
Nick Parkman <nicholaswparkman@gmail.com>
2025-07-24 20:51:53 -0700

    PopTheLock Update
    gameplay working, score, and lives system in place

---
+++ Assets/Scripts/PLT Scripts/PTLGameManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using TMPro;
+using UnityEngine;
+
+public class PTLGameManager : MonoBehaviour
+{
+    [SerializeField] private PTLLockpick lockpick;
+    [SerializeField] private GameObject unlockPoint;
+    [SerializeField] private float radius = 2.75f;
+    [SerializeField] private int points = 0;
+    [SerializeField] private int lives = 3;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI livesText;
+    [SerializeField] private bool gameOver = false;
+
+    private void Start()
+    {
+        points = 0;
+        lives = 3;
+        scoreText.text = "Score: " + points;
+        livesText.text = "Lives: " + lives;
+    }
+
+    private void Update()
+    {
+        if (!gameOver && Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject currentUnlockPoint = lockpick.GetCurrentUnlockPoint();
+            if (currentUnlockPoint != null)
+            {
+                Destroy(currentUnlockPoint);
+                SpawnUnlockPoint();
+                Score();
+            }
+            else
+            {
+                Damage();
+            }
+        }
+    }
+
+    private void SpawnUnlockPoint()
+    {
+        float angle = Random.Range(0f, 2 * Mathf.PI);
+        float x = radius * Mathf.Cos(angle);
+        float y = radius * Mathf.Sin(angle);
+        Vector3 spawnLocation = new Vector3(x, y, 0);
+        Instantiate(unlockPoint, spawnLocation, Quaternion.identity);
+    }
+
+    private void Score()
+    {
+        points++;
+        scoreText.text = "Score: " + points;
+    }
+
+    private void Damage()
+    {
+        lives--;
+        livesText.text = "Lives: " + lives;
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOver = true;
+        lockpick.gameObject.SetActive(false);
+        Debug.Log("Game Over");
+    }
+
+    public bool GetGameOver() { return gameOver; }
+}
---
+++ Assets/Scripts/PLT Scripts/PTLLockpick.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PTLLockpick : MonoBehaviour
+{
+    [SerializeField] private int rotationDirection = 1;
+    [SerializeField] private float rotationSpeed = 20.0f;
+    [SerializeField] private CapsuleCollider2D capsuleCollider;
+    [SerializeField] private GameObject currentUnlockPoint;
+
+    private void Start()
+    {
+        capsuleCollider = this.GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Update()
+    {
+        LockpickRotation();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rotationDirection = rotationDirection * -1;
+        }
+    }
+
+    private void LockpickRotation()
+    {
+        this.transform.RotateAround(Vector3.zero, new Vector3(0, 0, 1) * rotationDirection, rotationSpeed * Time.deltaTime);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentUnlockPoint = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        currentUnlockPoint = null;
+    }
+
+    public GameObject GetCurrentUnlockPoint() { return currentUnlockPoint; }
+    
+}
