28d46c226c52bf9c7a988a782cf762696e41e2e9
Nick Parkman <nicholaswparkman@gmail.com>
2025-08-10 15:55:27 -0700

    FB update
    Bird code start

--- Assets/Scripts/FB Scripts/FBBirdScript.cs
+++ Assets/Scripts/FB Scripts/FBBirdScript.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class FBBirdScript : MonoBehaviour
 {
-    
+    [SerializeField] private bool isAlive;
+    [SerializeField] private FBGameManager gameManager;
+    [SerializeField] private GameObject wingUp;
+    [SerializeField] private GameObject wingDown;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Vector2 flapVector = new Vector2(0f, 5f);
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(flapVector, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        int layerInt = collision.gameObject.layer;
+        if(layerInt == LayerMask.NameToLayer("Pipe")) SetIsAlive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerInt = collision.gameObject.layer;
+        if (layerInt == LayerMask.NameToLayer("Bounds"))
+            SetIsAlive(false);
+        else if (layerInt == LayerMask.NameToLayer("ScoreZone"))
+            gameManager.Score();
+    }
+
+    public bool GetIsAlive() { return isAlive; }
+
+    private void SetIsAlive(bool isAlive) { this.isAlive = isAlive; }
 }
--- Assets/Scripts/FB Scripts/FBGameManager.cs
+++ Assets/Scripts/FB Scripts/FBGameManager.cs
@@ -1,22 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FBGameManager : MonoBehaviour
 {
-    [SerializeField] private FBBirdScript Bird;
+    [SerializeField] private FBBirdScript bird;
     [SerializeField] private FBPipeSpawner pipeSpawner;
     [SerializeField] private bool gameGoing = false;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private int score = 0;
+    
+    public void Score() { score++; }
 
-    // Start is called before the first frame update
-    void Start()
+    private void ResetGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        score = 0;
     }
 }
